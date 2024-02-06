e9631ec0752c69d43c822d0e6c88e8fdb1f45453
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2024-02-06 08:23:01 +0530

    Created VMs and UI for posts

---
+++ app/src/main/java/com/consumers/socialmediademo/viewmodels/chatViewModel.kt
@@ -0,0 +1,9 @@
+package com.consumers.socialmediademo.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class chatViewModel : ViewModel() {
+
+}
